58bbfd315c89a8a7497987537b8bcd1ae2ace5d4
Ashik465 <helloashikul@gmail.com>
2023-08-12 02:51:27 +0600

    get cart done

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
+const jwt = require("jsonwebtoken");
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    // bearer token
+    const token = authorization.split(" ")[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, message: "unauthorized access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  };
 
 
 
@@ -29,6 +51,15 @@ async function run() {
     const productsCollection = client.db("shopWaveDb").collection("products");
     const cartCollection = client.db("shopWaveDb").collection("cart");
 
+    //   JWT Post
+    app.post("/jwt", (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+  
+        res.send({ token });
+      });
 
      //   Get products from database
      app.get("/products", async (req, res) => {
@@ -56,6 +87,28 @@ async function run() {
         res.send(result);
       });
 
+
+      //   Get cart
+    app.get("/cart", verifyJWT, async (req, res) => {
+        const email = req.decoded.email;
+        const userEmail = req.query.email;
+        if (email !== userEmail) {
+          return res
+            .status(401)
+            .send({ error: true, message: "unauthorized access" });
+        }
+        const query = { email: userEmail };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      });
+
+      //   Delete from cart
+    app.delete("/cart/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+      });
 
 
     // Send a ping to confirm a successful connection
