88e593a458fef66225a1da5fd57c5fd355a379bf
Ashik465 <helloashikul@gmail.com>
2023-08-12 15:36:03 +0600

    add customer to database

--- index.js
+++ index.js
@@ -125,6 +125,21 @@ async function run() {
         res.send(result);
       });
 
+       // Get all customers
+    app.get("/customers", async (req, res) => {
+        const query = { role: "customer" };
+        const result = await usersCollection.find(query).toArray();
+        res.send(result);
+      });
+
+       // Delete customer
+    app.delete("/deletecustomer/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
